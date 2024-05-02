d732d872023f941759a8fa218c7bdff799309702
LDHheh <l.mazurkevych@ortlyc.kiev.ua>
2024-05-02 09:40:22 +0000

    djn nfr djn

--- script.js
+++ script.js
@@ -3,35 +3,45 @@ let y = 300;
 let vx = 0; 
 let vy = 0; 
 
+let img;
+
+function preload() {
+  img = loadImage('да.jpg');
+}
 function setup() {
   createCanvas(400, 400);
+   
 }
 
 function draw() {
-  background(220);
-
+  //background(220);
+  image(img, 0, 0);
   fill(255, 0, 0);
-  ellipse(x, y, 20, 20);
+  ellipse(x, y, 40, 40); 
 
-  vx *= 0.95;
+  vx *= 0.99; 
   vy += 0.1; 
   x += vx;
   y += vy;
 
-
   if (y > height) {
     y = height;
-    }
-}
+    vy = 0;
+  }
 
+  if (x < 0 || x > width) {
+    vx = 0;
+  }
+}
 function keyPressed() {
-  if (key === ' ') {
-    vy = -5; 
-  }
-  if (key === '68') {
-    vx = -5;
-  }
-  if (key === '65') 
-    vx = 5;
+if (key === ' ') {
+  vy = -5; 
+}
+if (keyCode === LEFT_ARROW) {
+  vx = -5;
+}
+if (keyCode === RIGHT_ARROW) {
+  vx = 5;
+}
 
 }
